dc673caaea18722c305cac6abeec51b8c385b8e0
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2025-08-29 14:59:55 +0000

    ds

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Studio-Reform/',   // <-- add this line for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
-
