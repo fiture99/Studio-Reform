4699404d9bf3072ab54d266a974699cf5c8ba983
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2025-10-25 10:29:57 +0000

    sd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Studio-Reform/', 
+  base: '/', 
   server: {
     proxy: {
       '/api': {
